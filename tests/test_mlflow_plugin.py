68861c048a5fbb406117ae4a409461962b5538c2
Francisco Aranda <frascuchon@gmail.com>
2021-05-11 12:45:03 +0200

    Feature/deployment at endpoint level (#3)
    * feat(deploy): handle deployments at endpoint level instead model
    
    * chore: configure tests
    
    * chore: update readme

--- tests/test_mlflow_plugin.py
+++ tests/test_mlflow_plugin.py
@@ -20,28 +20,44 @@ class AddN(mlflow.pyfunc.PythonModel):
 
 
 # Construct and save the model
-model_path = os.path.abspath("models/add_n_model")
 
+model_5_path = os.path.abspath("models/add_5_model")
 try:
-    mlflow.pyfunc.save_model(path=model_path, python_model=AddN(n=5))
-except MlflowException as e:
+    mlflow.pyfunc.save_model(path=model_5_path, python_model=AddN(n=5))
+except MlflowException:
     pass
+
+model_6_path = os.path.abspath("models/add_6_model")
+try:
+    mlflow.pyfunc.save_model(path=model_6_path, python_model=AddN(n=6))
+except MlflowException:
+    pass
+
 # Evaluate the model
 client = get_deploy_client("ray-serve")
 
-client.delete_deployment("add5")
-client.create_deployment("add5", model_uri=model_path)
-print(client.list_deployments())
-print(client.get_deployment("add5"))
 
-model_input = pd.DataFrame([range(10)])
-expected_output = pd.DataFrame([range(5, 15)])
-model_output = client.predict("add5", model_input)
-assert model_output.equals(expected_output)
+try:
+    client.delete_deployment("addN")
+    client.create_deployment("addN", model_5_path)
+    print(client.list_deployments())
+    print(client.get_deployment("addN"))
 
-response = requests.post("http://localhost:8000/add5", json=model_input.to_json())
-assert response.status_code == 200
-assert pd.read_json(response.content).equals(expected_output)
+    model_input = pd.DataFrame([range(10)])
+    model_output = client.predict("addN", model_input)
+    assert model_output.equals(pd.DataFrame([range(5, 15)]))
+    
+    client.update_deployment("addN", model_uri=model_6_path, config={"model_traffic": 1.0})
+    print(client.get_deployment("addN"))
 
-client.delete_deployment("add5")
-shutil.rmtree(model_path)
+    model_input = pd.DataFrame([range(10)])
+    expected_output = pd.DataFrame([range(6, 16)])
+    model_output = client.predict("addN", model_input)
+    assert model_output.equals(expected_output)
+    
+    response = requests.post("http://localhost:8000/addN", json=model_input.to_json())
+    assert response.status_code == 200
+    assert pd.read_json(response.content).equals(expected_output)
+finally:
+    client.delete_deployment("addN")
+    shutil.rmtree(os.path.dirname(model_5_path))
